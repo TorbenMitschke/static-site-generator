73295568f905c472aabe9b4d4cb5bca3963abc9a
TorbenMitschke <torben.mitschke@gmail.com>
2024-07-11 10:20:30 +0200

    add text_type variables

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from textnode import TextNode
+from textnode import TextNode, text_type_italic
 
 def main():
-    text_node = TextNode("I am a text node", "italic", "https://www.markdownguide.org")
+    text_node = TextNode("I am a text node", text_type_italic, "https://www.markdownguide.org")
 
     print(text_node)
 
