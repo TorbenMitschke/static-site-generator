1d00da7f74638912c52f776cc5970d6cc9fbdbd1
TorbenMitschke <torben.mitschke@gmail.com>
2024-07-30 17:31:00 +0200

    add leafnode class and tests

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
-from textnode import TextNode, text_type_italic
+from textnode import TextNode, text_type_italic, text_type_link
+from leafnode import LeafNode
 
 def main():
-    text_node = TextNode("I am a text node", text_type_italic, "https://www.markdownguide.org")
+    text_node = TextNode("I am a text node", text_type_link, "https://www.markdownguide.org")
 
     print(text_node)
+
+    leafnode = LeafNode("a", "click here", {"href": "https://github.com/TorbenMitschke", "target": "_blank"})
+    leafnode2 = LeafNode(None, "click here")
+    print(leafnode.to_html())
+    print(leafnode2.to_html())
 
 main()
