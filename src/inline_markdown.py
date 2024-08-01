6d70fa94556d705f816ad13deed26e18a4c2c427
TorbenMitschke <torben.mitschke@gmail.com>
2024-08-01 17:44:57 +0200

    add functionality to convert md to textnode

---
+++ src/inline_markdown.py
@@ -0,0 +1,5 @@
+from textnode import TextType
+
+# TODO: Implement logic for function follwoing the local_debug approach
+def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: TextType) -> list:
+    return
