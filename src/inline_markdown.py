3064b60eb44a5de02f98c94013c719d0cdc06f6b
TorbenMitschke <torben.mitschke@gmail.com>
2024-08-02 15:30:48 +0200

    add split_nodes_delimiter function

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,5 +1,22 @@
-from textnode import TextType
+from textnode import TextNode, TextType
 
 # TODO: Implement logic for function follwoing the local_debug approach
 def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: TextType) -> list:
-    return
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        split_nodes = []
+        section = node.text.split(delimiter)
+        if len(section) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(node.text.split(delimiter))):
+            if section[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(section[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(section[i], text_type))
+        new_nodes.extend(split_nodes)
+    return new_nodes
